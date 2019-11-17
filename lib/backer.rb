160282a293cf00d2d98ac71feec260139707054f
deezy13 <deezy13@gmail.com>
2019-11-17 20:34:52 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,17 +1,4 @@
-class Backer
-  
-  attr_accessor :backed_projects
-  attr_reader :name 
-  
-  def initialize(name)
-    @name = name 
-    @backed_projects = []
-  end 
-  
+
   def back_project(project)
-    
-       @backed_projects << project
-        bac = Project.new(project)
-        bac.backers << self 
-  end 
-end
+        @backed_projects << project
+        project.add_backer(self) unless project.backers.include?(self)
