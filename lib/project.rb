8a794e21e20dd7ecdd681cb529f6d1e68bf0a921
deezy13 <deezy13@gmail.com>
2019-11-17 04:25:58 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,11 @@
 class Project
   
-  attr_accessor :backed_projects
+  attr_accessor :backers
   attr_reader :title 
   
   def initialize(title)
     @title = title
-    @backed_projects = []
+    @backers = []
   end 
   
   
